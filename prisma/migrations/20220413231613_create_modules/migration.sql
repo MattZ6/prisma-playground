fe9533310af8b7de968d6913faefe7f5da1ee015
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-04-13 20:18:57 -0300

    feat: add create modules migration

---
+++ prisma/migrations/20220413231613_create_modules/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "modules" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "duration_in_hours" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "modules_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "modules_name_key" ON "modules"("name");
